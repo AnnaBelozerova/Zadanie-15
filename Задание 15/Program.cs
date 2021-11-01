0bb0a6b0e25171f30d267f1037cebd9a83167113
Анна Белозерова <bel.denisovna@gmail.com>
2021-11-01 20:04:10 +0300

    Правки

--- Задание 15/Program.cs
+++ Задание 15/Program.cs
@@ -10,70 +10,154 @@ namespace Задание_15
     {
         static void Main(string[] args)
         {
-            
-            GeomProgression gp = new GeomProgression();
-            Console.Write("Введите начальное значени ряда: ");
-            
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите разность арифметического ряда: ");
-            int d = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите знаменатель геометрического ряда: ");
-            int q = Convert.ToInt32(Console.ReadLine());
-
-            ArithProgression ap = new ArithProgression(x,d);
-            for (int i = x; i < 10; i+=d)
+            try
             {
-                ap.getNext(i);
-                
+                Console.Write("Введите количество элементов ряда: ");
+                int chet = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите начальное значени ряда: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите разность арифметического ряда: ");
+                int d = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите знаменатель геометрического ряда: ");
+                int q = Convert.ToInt32(Console.ReadLine());
+
+                ArithProgression arithProgression = new ArithProgression(x, d);
+
+                Console.Write(arithProgression.getStart());
+                for (int i = 0; i < chet; i++)
+                {
+                    Console.Write("{0,8}", arithProgression.getNext());
+                }
+                Console.WriteLine();
+
+                GeomProgression geomProgression = new GeomProgression(x, q);
+
+                Console.Write(geomProgression.getStart());
+                for (int i = 0; i < chet; i++)
+                {
+                    Console.Write("{0,8}", geomProgression.getNext());
+                }
+                Console.WriteLine();
+
+                Console.Write("Введите новое начальное значени ряда: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                arithProgression.Resert();
+                arithProgression.setStart(x);
+                Console.Write(arithProgression.getStart());
+                for (int i = 0; i < chet; i++)
+                {
+                    Console.Write("{0,8}", arithProgression.getNext());
+                }
+                Console.WriteLine();
+
+                geomProgression.Resert();
+                geomProgression.setStart(x);
+                Console.Write(geomProgression.getStart());
+                for (int i = 0; i < chet; i++)
+                {
+                    Console.Write("{0,8}", geomProgression.getNext());
+                }
+                Console.WriteLine();
             }
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.ReadKey();
+
         }
     }
     interface ISeries
     {
         void setStart(int x);
-        int getNext(int x);
+        int getNext();
         void Resert();
 
     }
     public class ArithProgression : ISeries
     {
-        int x;
+        int start;
+        int value;
         int d;
-        public ArithProgression(int x, int d)
-        { 
-        }
-        public int getNext(int x)
+        public ArithProgression(int start=0, int d=1)
         {
-            return x + d;
+            this.start = start;
+            value = start;
+            this.d = d;
+        }
+        public int getNext()
+        {
+            value += d;
+            return value;
         }
 
         public void Resert()
         {
-            throw new NotImplementedException();
+            value = start;
         }
 
         public void setStart(int x)
         {
-            Console.WriteLine(x);
+            this.start = x;
+            value = this.start;
+        }
+        public int getStart()
+        {
+            return this.start;
         }
     }
     class GeomProgression : ISeries
     {
-        int x;
-        public int getNext(int d)
+        int start;
+        int value;
+        int q;
+        public GeomProgression(int start=1, int q=2)
         {
-            return x * d;
+            if (start != 0 && q != 0)
+            {
+                this.start = start;
+                value = start;
+                this.q = q;
+            }
+            else
+            {
+                if (start == 0)
+                {
+                    this.start = 1;
+                    value = 1;
+                    this.q = q;
+                    Console.WriteLine("Начальное значение геометрического ряда не может быть равно 0. Установлено значение по умолчанию.");
+                }
+                else
+                {
+                    this.start = start;
+                    value = start;
+                    this.q = 2;
+                    Console.WriteLine("Значение знаменателя геометрического ряда не может быть равно 0 или 1. Установлено значение по умолчанию.");
+                }
+            }
+            
+        }
+        public int getNext()
+        {
+            value *= q;
+            return value;
         }
 
         public void Resert()
         {
-            throw new NotImplementedException();
+            value = start;
         }
 
         public void setStart(int x)
         {
-            throw new NotImplementedException();
+            this.start = x;
+            value = this.start;
+        }
+        public int getStart()
+        {
+            return this.start;
         }
     }
 }
